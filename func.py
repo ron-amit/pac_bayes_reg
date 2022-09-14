71c10f7f8d8a3815a47a41850d3ccd1213024b6e
ronamit <ronamit@campus.technnion.ac.il>
2022-09-14 16:42:51 +0300

    .

--- func.py
+++ func.py
@@ -1,9 +1,47 @@
 import torch
-from torch import sqrt, log
+from torch import tensor
+from math import sqrt, log, pi
 
+def loss(h: tensor,x: tensor,y: tensor):
+    assert h.n_dim == 1
+    assert x.n_dim == 2  # [n_samp x d]
+    assert y.n_dim == 1
+    assert y.shape[0] == x.shape[0]
+    return torch.square(torch.matmul(x, h) - y) / 4
 
-def uc_bound(m, delta, d):
+def uc_bound(m: int, delta: float, d: int):
     assert delta > 0
-    bnd = sqrt(log((6 / delta) / (32 * m))) + sqrt((d * (1 + 2 * sqrt(3 / delta)) + 2 * log(3 / delta)) / (4 * m)) \
-          + 00000000000000000000
+    tmp1 = (5 * d + 2 * log(6 / delta)) / m
+    bnd = sqrt(log((6 / delta) / (32 * m))) + sqrt((d + 2 * d * sqrt(log(3 / delta)) + 2 * log(3 / delta)) / (4 * m)) \
+          + 8 * max(sqrt(tmp1), tmp1)
+    return bnd
+
+
+def uc_grad_bound(m: int, delta: float, d: int, r: float):
+    assert delta > 0
+    tmp1 = (5 * d + 2 * log(4 / delta)) / m
+    bnd = 16 * r * max(sqrt(tmp1), tmp1) + r * sqrt((d + 2 * d * sqrt(log(2 / delta)) + 2 * log(2 / delta)) / (4 * m))
+    return bnd
+
+
+def wasserstein_gauss_proj(mu_q: tensor, mu_p: tensor, sigma_q: tensor, sigma_p: tensor, d: int,
+                           r: float):
+    assert mu_q.n_dim == 1
+    assert mu_q.shape[0] == d
+    assert sigma_q.n_elem == 1
+    assert sigma_p.n_elem == 1
+    w_bnd = torch.sqrt(torch.sum((mu_q - mu_p) ** 2) + d * (sigma_q - sigma_p) ** 2) \
+            + sqrt(pi / 2) * sigma_q * torch.erfc(
+        (r - torch.sqrt(torch.sum(mu_q ** 2) + d * sigma_q ** 2)) / (sqrt(2) * sigma_q)) \
+            + sqrt(pi / 2) * sigma_p * torch.erfc(
+        (r - torch.sqrt(torch.sum(mu_p ** 2) + d * sigma_p ** 2)) / (sqrt(2) * sigma_p))
+    return w_bnd
+
+
+def wpb_bound(m: int, delta: float, mu_q: tensor, mu_p: tensor, sigma_q: tensor, sigma_p: tensor, d: int, r: float):
+    assert m > 1
+    u = uc_bound(m, delta / 4, d)
+    ug = uc_grad_bound(m, delta / 4, d, r)
+    w_bnd = wasserstein_gauss_proj(mu_q, mu_p, sigma_q, sigma_p, d, r)
+    bnd = torch.sqrt(2 * u * ug * w_bnd + log(2 * m / delta) / (2 * (m - 1)))
     return bnd
